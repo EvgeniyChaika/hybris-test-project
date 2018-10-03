bfd0a12dde9725d9d9a606e2ac91af60b74e9570
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2018-10-03 23:27:14 +0300

    Write the business logic for a cron job

---
+++ hybris/bin/custom/concerttours/src/concerttours/service/NewsService.java
@@ -0,0 +1,11 @@
+package concerttours.service;
+
+import java.util.Date;
+import java.util.List;
+
+import concerttours.model.NewsModel;
+
+public interface NewsService {
+
+    List<NewsModel> getNewsOfTheDay(Date date);
+}
